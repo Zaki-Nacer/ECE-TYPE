667b2e83b7e0d8a6fe5f45001fa0bf9eaee336b1
Zibike <joseph.zibimeyer@edu.ece.fr>
2025-05-05 10:48:07 +0200

    Add files via upload

---
+++ hud (1).h
@@ -0,0 +1,19 @@
+#ifndef HUD_H
+#define HUD_H
+
+#include "defs.h" // Pour GameState
+
+// --- Prototypes des Fonctions HUD ---
+
+// Charge les ressources HUD DANS gameState
+void charger_ressources_hud(GameState *gameState);
+
+// Met à jour l'état du HUD DANS gameState
+void mettre_a_jour_hud(GameState *gameState);
+
+// Dessine une barre de progression (utilise les sprites de gameState)
+void dessiner_barre_progression(GameState *gameState, BITMAP *dest, int x, int y, int value, int max_value);
+
+// Dessine l'ensemble du HUD (depuis gameState) sur le buffer (gameState->buffer)
+void dessiner_hud(GameState *gameState);
+#endif // HUD_H
